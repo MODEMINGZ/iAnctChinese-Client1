19e4060069a0690c1a8a874d39d100f020747847
余准 <108219118+MODEMINGZ@users.noreply.github.com>
2024-09-23 15:06:28 +0800

    完成首页，项目管理页初步布局

--- iAnctChinese-Client1/src/views/Home.vue
+++ iAnctChinese-Client1/src/views/Home.vue
@@ -14,38 +14,45 @@
     </el-menu>
     <div class="content">
         <el-carousel trigger="click" arrow="always" indicator-position="outside" :autoplay="false" ref="carousel"
-            class="carousel" @change="handleCarouselChange">
+            class="carousel" @change="handleCarouselChange" height="auto">
             <el-carousel-item class="el-carousel-item">
                 <div class="textbox">
                     <h1>iAnctChinese-古汉语智能标注平台</h1>
-                    <p class="text"> iAnctChinese 是专为古汉语设计的智能标注平台，集成了pytorch框架下的先进深度学习模型，为研究者与学者提供了一站式的古汉语文本处理解决方案。
+                    <p class="text"> iAnctChinese
+                        是<strong>专为古汉语设计的智能标注平台</strong>，集成了pytorch框架下的先进深度学习模型，为研究者与学者提供了一站式的古汉语文本处理解决方案。
                         平台支持对古汉语自由文本的智能化结构整理、实体标注、关系标注，并自动生成知识图谱。
                         <br>通过精巧的交互设计，iAnctChinese实现了高效的人机协作，不仅在古汉语文本上实现了高精度的实体识别与关系抽取，未来还将不断拓展，支持更多语言的处理，为数字人文研究注入新的活力。
                     </p>
                 </div>
             </el-carousel-item>
-            <el-carousel-item>
+            <el-carousel-item class="el-carousel-item">
                 <div class="textbox">
                     <h1>主要功能</h1>
-                    <p class="text"> iAnctChinese 是专为古汉语设计的智能标注平台，集成了pytorch框架下的先进深度学习模型，为研究者与学者提供了一站式的古汉语文本处理解决方案。
-                        平台支持对古汉语自由文本的智能化结构整理、实体标注、关系标注，并自动生成知识图谱。
-                        <br>通过精巧的交互设计，iAnctChinese实现了高效的人机协作，不仅在古汉语文本上实现了高精度的实体识别与关系抽取，未来还将不断拓展，支持更多语言的处理，为数字人文研究注入新的活力。
+                    <p class="text">
+                        1. <strong>智能化结构整理：</strong> iAnctChinese 平台能够自动对古汉语自由文本进行结构整理，将其转化为计算机可理解的格式，为后续处理提供便利。<br>
+                        2. <strong>实体标注：</strong> 平台内置先进的深度学习模型，能够准确识别并标注出文本中的实体，如人名、地名、机构名等，有助于用户快速理解文本内容。<br>
+                        3. <strong>关系标注：</strong> 除了实体标注外，iAnctChinese 还能自动标注出实体之间的关系，揭示文本中隐含的信息，为用户提供更丰富的知识。<br>
+                        4. <strong>知识图谱生成：</strong> 基于实体标注和关系标注的结果，平台能够自动生成知识图谱，以图形化的方式展示文本中的知识结构，便于用户进行深入分析和挖掘。
                     </p>
                 </div>
             </el-carousel-item>
-            <el-carousel-item>
+            <el-carousel-item class="el-carousel-item">
                 <div class="textbox">
                     <h1>主要特色</h1>
-                    <p class="text"> iAnctChinese 是专为古汉语设计的智能标注平台，集成了pytorch框架下的先进深度学习模型，为研究者与学者提供了一站式的古汉语文本处理解决方案。
-                        平台支持对古汉语自由文本的智能化结构整理、实体标注、关系标注，并自动生成知识图谱。
-                        <br>通过精巧的交互设计，iAnctChinese实现了高效的人机协作，不仅在古汉语文本上实现了高精度的实体识别与关系抽取，未来还将不断拓展，支持更多语言的处理，为数字人文研究注入新的活力。
+                    <p class="text">
+                        1. <strong>专为古汉语设计：</strong> iAnctChinese
+                        平台针对古汉语的特点进行专门设计，充分考虑了古汉语的语法、词汇和表达方式，确保处理结果的准确性和可靠性。<br>
+                        2. <strong>一站式解决方案：</strong> 平台提供了从文本整理到知识图谱生成的一站式服务，用户无需跳转至其他工具或平台，即可完成全部处理流程。<br>
+                        3. <strong>高效的人机协作：</strong> 通过精巧的交互设计，iAnctChinese
+                        实现了高效的人机协作。用户可以在平台上轻松进行参数调整、结果查看和反馈修正等操作，提升工作效率。<br>
+                        4. <strong>可扩展性强：</strong> 平台不仅支持古汉语的处理，未来还将不断拓展，支持更多语言的处理。同时，用户还可以根据自身需求定制开发新功能，满足多样化的应用场景。
                     </p>
                 </div>
             </el-carousel-item>
         </el-carousel>
         <div class="btnbox">
-            <el-button type="primary" class="btn">开始使用</el-button>
-            <el-button class="btn">帮助中心</el-button>
+            <el-button type="primary" class="btn" @click="goStart">开始使用</el-button>
+            <el-button class="btn">帮助</el-button>
         </div>
 
     </div>
@@ -59,15 +66,21 @@ import {
     InfoFilled
 } from '@element-plus/icons-vue'
 import { useRoute, useRouter } from 'vue-router'
-//转去关于页面
 const route = useRoute()
 const router = useRouter()
+//开始使用
+const goStart = () => {
+    router.push('/project')
+}
+//转去关于页面
+
 const goAbout = () => {
     router.push('/about')
 }
 //菜单项和走马灯联动
 const activeIndex = ref(1);
-const carousel = ref(null);
+const carousel: any = ref()
+// const carousel = ref(null);
 const handleMenuSelect = (index: string) => {
     const carouselIndex = parseInt(index) - 1;
     if (carouselIndex > 2) {
@@ -89,14 +102,16 @@ const handleCarouselChange = (index: number) => {
     display: flex;
     justify-content: center;
     align-items: center;
-    /* height: 100%; */
+    /* background-color: aqua; */
+    height: 400px;
 }
 
 .carousel {
     /* background-color: #98a7f8; */
     /* align-items: center; */
-    height: auto;
-    margin-top: 10%;
+    /* height: 500px; */
+    margin-top: 5%;
+    padding: 10px;
     width: 100%;
 }
 
--- iAnctChinese-Client1/src/views/Project.vue
+++ iAnctChinese-Client1/src/views/Project.vue
@@ -1,11 +1,146 @@
 <template>
-    <div>
-
+    <div class="container">
+        <div class="main-app">
+            <div class="header">
+                <div class="head">
+                    <el-icon class="head-icon">
+                        <FolderOpened />
+                    </el-icon>
+                    <span class="head-text">项目管理</span>
+                </div>
+                <div class="right-head">
+                    <el-button type="primary" class="h-btn" :icon="FolderAdd">新建项目</el-button>
+                    <el-button type="danger" :icon="Delete" class="h-btn">删除项目</el-button>
+                </div>
+            </div>
+            <el-table :data="tableData" class="mtable">
+                <el-table-column prop="name" label="Name" width="120" />
+                <el-table-column fixed prop="date" label="Date" width="150" />
+                <el-table-column fixed="right" label="Operations" min-width="120">
+                    <template #default="scope">
+                        <el-button link type="primary" size="small" @click.prevent="deleteRow(scope.$index)">
+                            Remove
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import {
+    FolderAdd,
+    FolderOpened,
+    Delete
+} from '@element-plus/icons-vue'
 
+//表格
+import { ref } from 'vue'
+import dayjs from 'dayjs'
+
+const now = new Date()
+
+const tableData = ref([
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+    },
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+    },
+])
+
+const deleteRow = (index: number) => {
+    tableData.value.splice(index, 1)
+}
+
+const onAddItem = () => {
+    now.setDate(now.getDate() + 1)
+    tableData.value.push({
+        date: dayjs(now).format('YYYY-MM-DD'),
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+    })
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.mtable {
+    background-color: aqua;
+    margin-left: 20px;
+    margin-top: 10px;
+}
+
+.h-btn {
+    margin-right: 10px;
+}
+
+.right-head {
+
+    height: 100%;
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+
+}
+
+.head {
+    display: flex;
+    margin-left: 20px;
+    align-items: center;
+}
+
+.head-icon {
+
+    font-size: 30px;
+    font-weight: 700;
+}
+
+.head-text {
+    font-size: 24px;
+    font-weight: 700;
+}
+
+.header {
+    display: flex;
+    height: 50px;
+    border-bottom: 1px solid grey;
+    align-items: center;
+    padding: 10px 20px 10px 10px;
+    justify-content: space-between;
+}
+
+
+.main-app {
+    background-color: white;
+    height: 100%;
+    border-radius: 10px;
+    /* width: 100%; */
+}
+
+.container {
+    background-color: #f6f6f6;
+    height: 100vh;
+    padding: 10px;
+}
+</style>
